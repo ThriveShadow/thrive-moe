d7be417a57c16c1d509919ab7d9dc012396fdc96
ThriveShadow <jasononggt@gmail.com>
2023-02-26 21:04:00 +0700

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
-
-import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
